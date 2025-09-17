ff60d90673712821820179a50a04a1c46a8c7b03
Toni Daada <tonidaada@gmail.com>
2025-09-17 02:41:07 +0100

    "Adde Product list and used smaple data, then responsivess i.e the sheet" 	modified:   app/(root)/page.tsx 	modified:   components/shared/header/index.tsx new file:   components/shared/header/menu.tsx new file:   components/shared/product/product-list.tsx new file:   components/ui/sheet.tsx new file:   db/sample-data.ts 	modified:   package-lock.json 	modified:   package.json

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,24 @@
 import { Button } from "@/components/ui/button";
 import LoadingPage from "../loading";
 
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+
 export const metadata = {
   title: "Home",
   description: "Welcome to Prostore, your one-stop shop for everything!",
 };
 
 const HomePage = async () => {
-  return <>HomePage</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
