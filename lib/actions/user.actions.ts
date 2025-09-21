f47ee76d62ce4ca6ae21f27c176a5719773fbd40
Toni Daada <tonidaada@gmail.com>
2025-09-21 04:08:05 +0100

    "Created the sign in validtors using zod schema, created the sign-in route and form" 	new file:   app/(auth)/layout.tsx 	new file:   app/(auth)/sign-in/credentials-signin-form.tsx 	new file:   app/(auth)/sign-in/page.tsx 	new file:   app/api/auth/[...nextauth]/route.ts 	new file:   auth.ts 	new file:   components/ui/input.tsx 	new file:   components/ui/label.tsx 	new file:   lib/actions/user.actions.ts 	modified:   lib/constants/index.ts 	modified:   lib/validator.ts 	modified:   package-lock.json 	modified:   package.json

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,31 @@
+"use server";
+// Bacekend code
+import { signInFormSchema } from "../validator";
+import { signIn, signOut } from "@/auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+
+//Sign In the user with credentials
+
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+    await signIn("credentials", user);
+    return { success: true, message: "Sign In successfully" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { success: false, message: "Invalid email or password" };
+  }
+}
+
+//Sign user out
+export async function signOutUser() {
+  await signOut();
+}
--- lib/validator.ts
+++ lib/validator.ts
@@ -26,3 +26,9 @@ export const insertProductsSchema = z.object({
   banner: z.string().nullable(),
   price: currency,
 });
+
+// Schema for stigning users in
+export const signInFormSchema = z.object({
+  email: z.email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 charracters"),
+});
