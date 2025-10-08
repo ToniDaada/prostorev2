0d2dec48c995c57fb119080865f8bb2d3dfa17a2
Toni Daada <tonidaada@gmail.com>
2025-10-08 02:31:16 +0100

    "Created rthe shipping adress form, hooked it with useForm and hook/form resolvers" 	modified:   app/(root)/shipping-address/shipping-address-form.tsx 	modified:   lib/validator.ts

--- lib/validator.ts
+++ lib/validator.ts
@@ -71,7 +71,7 @@ export const insertCartSchema = z.object({
 
 export const shippingAddressSchema = z.object({
   fullName: z.string().min(3, "Name must be at least 3 characters"),
-  streetAddress: z.string().min(3, "Address must be at leasr 3 characters"),
+  streetAddress: z.string().min(3, "Address must be at least 3 characters"),
   city: z.string().min(3, "City must be at leasr 3 characters"),
   postalCode: z.string().min(3, "Postal Code must be at leasr 3 characters"),
   country: z.string().min(3, "Country must be at leasr 3 characters"),
