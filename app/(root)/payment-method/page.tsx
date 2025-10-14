7f112188fac04af286258f7d6b2515ee58fa42d3
Toni Daada <tonidaada@gmail.com>
2025-10-14 02:03:35 +0100

    'Payment Method Form' 	modified:   app/(root)/payment-method/page.tsx 	new file:   app/(root)/payment-method/payment-method-form.tsx new file:   components/ui/radio-group.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,5 +1,26 @@
-const PaymentMethodPage = () => {
-  return <>Payment Method</>;
+import { Metadata } from "next";
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import PaymentMethodForm from "./payment-method-form";
+import CheckoutSteps from "@/components/shared/checkout-steps";
+
+export const metadata: Metadata = {
+  title: "Select Payment Method",
+};
+const PaymentMethodPage = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+  if (!userId) throw new Error("No user ID");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <CheckoutSteps currentStep={2} />
+
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod || ""} />
+    </>
+  );
 };
 
 export default PaymentMethodPage;
